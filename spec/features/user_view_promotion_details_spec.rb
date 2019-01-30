be64902711a3f94950acf76d34418f48094c8527
Everton Quadros <evertonquadros15@gmail.com>
2019-01-29 23:27:52 -0200

    Usuário visualiza detalhes da promoção

---
+++ spec/features/user_view_promotion_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User view promotion details' do
+  scenario 'successfully' do
+    promotion = create(:promotion)
+
+    login_user
+    visit root_path
+    click_on promotion.title
+
+    expect(current_path).to eq promotion_path(promotion)
+    expect(page).to have_content(promotion.title)
+    expect(page).to have_content(promotion.description)
+    expect(page).to have_content('Desconto:')
+    expect(page).to have_content(promotion.discount)
+    expect(page).to have_content('Início:')
+    expect(page).to have_content(promotion.start_at)
+    expect(page).to have_content('Expira em:')
+    expect(page).to have_content(promotion.end_at)
+    expect(page).to have_content('Prefixo:')
+    expect(page).to have_content(promotion.prefix)
+    expect(page).to have_content('Qtde cupons:')
+    expect(page).to have_content(promotion.quantity)
+    expect(page).to have_content('Duração:')
+    expect(page).to have_content(promotion.duration)
+  end
+end
